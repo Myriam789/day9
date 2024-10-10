7b89ddbf4bde370b5dea5cb4a5d27501fde473b8
Myriam789 <myriam.sall@epitech.digital>
2024-10-10 19:59:50 +0200

    ex_03.js

---
+++ ex_03.js
@@ -0,0 +1,12 @@
+function generateCards {
+
+        const container = document.getElementById('cardContainer');
+        container.innerHTML='';
+
+    }
+    data.forEach(element => {
+        const card = document.createElement('div')
+        card.className = 'card';
+        card.innerHTML = '
+        <
+  });
